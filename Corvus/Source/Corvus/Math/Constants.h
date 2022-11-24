6312eb00e0585518aa0a325a3c6b802ff309ba01
Slava Tenyaev <61147580+sltn011@users.noreply.github.com>
2022-11-24 21:28:22 +0300

    Float comparison + fixed crash on hiding window

---
+++ Corvus/Source/Corvus/Math/Constants.h
@@ -0,0 +1,16 @@
+#ifndef CORVUS_SOURCE_CORVUS_MATH_CONSTANTS_H
+#define CORVUS_SOURCE_CORVUS_MATH_CONSTANTS_H
+
+#include "Corvus/Math/Vector.h"
+
+namespace Corvus::Constants
+{
+
+    static inline constexpr float Pi = 3.14159265359f;
+
+    static inline constexpr float SmallNum     = 0.001f;
+    static inline constexpr float VerySmallNum = 0.000001f;
+
+} // namespace Corvus::Constants
+
+#endif //! CORVUS_SOURCE_CORVUS_MATH_CONSTANTS_H
